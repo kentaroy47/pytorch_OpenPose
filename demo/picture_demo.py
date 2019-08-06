10ed6594dfdffe27a560f311bf08dde169582c75
An Wangpeng <tensorboy@aifi.io>
2019-08-06 14:56:14 +0800

    add C++ postprorcessing

--- demo/picture_demo.py
+++ demo/picture_demo.py
@@ -14,7 +14,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from collections import OrderedDict
-from scipy.ndimage.filters import gaussian_filter
 from network.rtpose_vgg import get_model
 from network.post import decode_pose
 from training.datasets.coco_data.preprocessing import (inception_preprocess,
@@ -22,8 +21,52 @@ from training.datasets.coco_data.preprocessing import (inception_preprocess,
                                               ssd_preprocess, vgg_preprocess)
 from network import im_transform
 from evaluate.coco_eval import get_multiplier, get_outputs, handle_paf_and_heat
+from utils.common import Human, BodyPart, CocoPart, CocoColors, CocoPairsRender
+from pafprocess import pafprocess
+from scipy.ndimage.filters import gaussian_filter, maximum_filter
+from scipy.ndimage.morphology import generate_binary_structure
 
+def find_peaks(img):
+    """
+    Given a (grayscale) image, find local maxima whose value is above a given
+    threshold (param['thre1'])
+    :param img: Input image (2d array) where we want to find peaks
+    :return: 2d np.array containing the [x,y] coordinates of each peak found
+    in the image
+    """
 
+    peaks_binary = (maximum_filter(img, footprint=generate_binary_structure(
+        2, 1)) == img) * (img > 0.1)
+    out = np.zeros_like(img)
+    out[peaks_binary] = img[peaks_binary]
+    return out
+
+def draw_humans(npimg, humans, imgcopy=False):
+    if imgcopy:
+        npimg = np.copy(npimg)
+    image_h, image_w = npimg.shape[:2]
+    centers = {}
+    for human in humans:
+        # draw point
+        for i in range(CocoPart.Background.value):
+            if i not in human.body_parts.keys():
+                continue
+
+            body_part = human.body_parts[i]
+            center = (int(body_part.x * image_w + 0.5), int(body_part.y * image_h + 0.5))
+            centers[i] = center
+            cv2.circle(npimg, center, 3, CocoColors[i], thickness=3, lineType=8, shift=0)
+
+        # draw line
+        for pair_order, pair in enumerate(CocoPairsRender):
+            if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
+                continue
+
+            # npimg = cv2.line(npimg, centers[pair[0]], centers[pair[1]], common.CocoColors[pair_order], 3)
+            cv2.line(npimg, centers[pair[0]], centers[pair[1]], CocoColors[pair_order], 3)
+
+    return npimg
+        
 weight_name = './network/weight/pose_model.pth'
 
 model = get_model('vgg19')     
@@ -31,8 +74,6 @@ model.load_state_dict(torch.load(weight_name))
 model = torch.nn.DataParallel(model).cuda()
 model.float()
 model.eval()
-
-
 
 test_image = './readme/ski.jpg'
 oriImg = cv2.imread(test_image) # B,G,R order
@@ -54,9 +95,39 @@ with torch.no_grad():
     paf, heatmap = handle_paf_and_heat(
         orig_heat, flipped_heat, orig_paf, flipped_paf)
             
-param = {'thre1': 0.1, 'thre2': 0.05, 'thre3': 0.5}
-canvas, to_plot, candidate, subset = decode_pose(
-    oriImg, param, heatmap, paf)
- 
-cv2.imwrite('result.png',to_plot)   
+heatmap_peaks = np.zeros_like(heatmap)
+for i in range(19):
+    heatmap_peaks[:,:,i] = find_peaks(heatmap[:,:,i])
+heatmap_peaks = heatmap_peaks.astype(np.float32)
+heatmap = heatmap.astype(np.float32)
+paf = paf.astype(np.float32)
+
+#C++ postprocessing      
+pafprocess.process_paf(heatmap_peaks, heatmap, paf)
+
+humans = []
+for human_id in range(pafprocess.get_num_humans()):
+    human = Human([])
+    is_added = False
+
+    for part_idx in range(18):
+        c_idx = int(pafprocess.get_part_cid(human_id, part_idx))
+        if c_idx < 0:
+            continue
+
+        is_added = True
+        human.body_parts[part_idx] = BodyPart(
+            '%d-%d' % (human_id, part_idx), part_idx,
+            float(pafprocess.get_part_x(c_idx)) / heatmap.shape[1],
+            float(pafprocess.get_part_y(c_idx)) / heatmap.shape[0],
+            pafprocess.get_part_score(c_idx)
+        )
+
+    if is_added:
+        score = pafprocess.get_score(human_id)
+        human.score = score
+        humans.append(human)
+        
+out = draw_humans(oriImg, humans)
+cv2.imwrite('result.png',out)   
 
--- demo/web_demo.py
+++ demo/web_demo.py
@@ -23,11 +23,52 @@ from training.datasets.coco_data.preprocessing import (inception_preprocess,
                                               ssd_preprocess, vgg_preprocess)
 from network import im_transform
 from evaluate.coco_eval import get_multiplier, get_outputs
-#parser = argparse.ArgumentParser()
-#parser.add_argument('--t7_file', required=True)
-#parser.add_argument('--pth_file', required=True)
-#args = parser.parse_args()
+from utils.common import Human, BodyPart, CocoPart, CocoColors, CocoPairsRender
+from pafprocess import pafprocess
+from scipy.ndimage.filters import gaussian_filter, maximum_filter
+from scipy.ndimage.morphology import generate_binary_structure
 
+def find_peaks(img):
+    """
+    Given a (grayscale) image, find local maxima whose value is above a given
+    threshold (param['thre1'])
+    :param img: Input image (2d array) where we want to find peaks
+    :return: 2d np.array containing the [x,y] coordinates of each peak found
+    in the image
+    """
+
+    peaks_binary = (maximum_filter(img, footprint=generate_binary_structure(
+        2, 1)) == img) * (img > 0.1)
+    out = np.zeros_like(img)
+    out[peaks_binary] = img[peaks_binary]
+    return out
+
+def draw_humans(npimg, humans, imgcopy=False):
+    if imgcopy:
+        npimg = np.copy(npimg)
+    image_h, image_w = npimg.shape[:2]
+    centers = {}
+    for human in humans:
+        # draw point
+        for i in range(CocoPart.Background.value):
+            if i not in human.body_parts.keys():
+                continue
+
+            body_part = human.body_parts[i]
+            center = (int(body_part.x * image_w + 0.5), int(body_part.y * image_h + 0.5))
+            centers[i] = center
+            cv2.circle(npimg, center, 3, CocoColors[i], thickness=3, lineType=8, shift=0)
+
+        # draw line
+        for pair_order, pair in enumerate(CocoPairsRender):
+            if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
+                continue
+
+            # npimg = cv2.line(npimg, centers[pair[0]], centers[pair[1]], common.CocoColors[pair_order], 3)
+            cv2.line(npimg, centers[pair[0]], centers[pair[1]], CocoColors[pair_order], 3)
+
+    return npimg
+    
 weight_name = './network/weight/pose_model.pth'
 model = get_model('vgg19')     
 model.load_state_dict(torch.load(weight_name))
@@ -52,13 +93,43 @@ if __name__ == "__main__":
             paf, heatmap = get_outputs(
                 multiplier, oriImg, model,  'rtpose')
                   
+    heatmap_peaks = np.zeros_like(heatmap)
+    for i in range(19):
+        heatmap_peaks[:,:,i] = find_peaks(heatmap[:,:,i])
+    heatmap_peaks = heatmap_peaks.astype(np.float32)
+    heatmap = heatmap.astype(np.float32)
+    paf = paf.astype(np.float32)
 
-        param = {'thre1': 0.1, 'thre2': 0.05, 'thre3': 0.5}
-        canvas, to_plot, candidate, subset = decode_pose(
-            oriImg, param, heatmap, paf)
+    #C++ postprocessing      
+    pafprocess.process_paf(heatmap_peaks, heatmap, paf)
+
+    humans = []
+    for human_id in range(pafprocess.get_num_humans()):
+        human = Human([])
+        is_added = False
+
+        for part_idx in range(18):
+            c_idx = int(pafprocess.get_part_cid(human_id, part_idx))
+            if c_idx < 0:
+                continue
+
+            is_added = True
+            human.body_parts[part_idx] = BodyPart(
+                '%d-%d' % (human_id, part_idx), part_idx,
+                float(pafprocess.get_part_x(c_idx)) / heatmap.shape[1],
+                float(pafprocess.get_part_y(c_idx)) / heatmap.shape[0],
+                pafprocess.get_part_score(c_idx)
+            )
+
+        if is_added:
+            score = pafprocess.get_score(human_id)
+            human.score = score
+            humans.append(human)
+            
+    out = draw_humans(oriImg, humans)
 
         # Display the resulting frame
-        cv2.imshow('Video', to_plot)
+        cv2.imshow('Video', out)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
