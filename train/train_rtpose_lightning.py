80589f2ff92fcb47218e40467cde0bfed3685c80
An Wangpeng <tensorboy@aifi.io>
2019-08-09 11:03:11 +0800

    try lightning

--- train/train_rtpose_lightning.py
+++ train/train_rtpose_lightning.py
@@ -165,10 +165,10 @@ class rtpose_lightning(pl.LightningModule):
         loss_dict['tng_loss'] = total_loss
         loss_dict['batch_nb'] = batch_nb
         
-        loss_dict['max_heatmap'] = torch.max(pred2.data[:, :-1, :, :]).item()
-        loss_dict['min_heatmap'] = torch.min(pred2.data[:, :-1, :, :]).item()
-        loss_dict['max_paf'] = torch.max(pred1.data).item()
-        loss_dict['min_paf'] = torch.min(pred1.data).item()
+        #loss_dict['max_heatmap'] = torch.max(pred2.data[:, :-1, :, :]).item()
+        #loss_dict['min_heatmap'] = torch.min(pred2.data[:, :-1, :, :]).item()
+        #loss_dict['max_paf'] = torch.max(pred1.data).item()
+        #loss_dict['min_paf'] = torch.min(pred1.data).item()
         
         output = {
             'loss': total_loss, # required
