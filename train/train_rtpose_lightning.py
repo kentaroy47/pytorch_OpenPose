f7fef8bef3e47b60ceca346f765828b78b39bb83
tensorboy <anwangpeng@gmail.com>
2019-08-15 14:56:47 +0800

    reformat code

--- train/train_rtpose_lightning.py
+++ train/train_rtpose_lightning.py
@@ -20,8 +20,10 @@ from lib.config import cfg, update_config
 
 DATA_DIR = '/data/coco'
 
-ANNOTATIONS_TRAIN = [os.path.join(DATA_DIR, 'annotations', item) for item in ['person_keypoints_train2017.json']]
-ANNOTATIONS_VAL = os.path.join(DATA_DIR, 'annotations', 'person_keypoints_val2017.json')
+ANNOTATIONS_TRAIN = [os.path.join(DATA_DIR, 'annotations', item) for item in [
+    'person_keypoints_train2017.json']]
+ANNOTATIONS_VAL = os.path.join(
+    DATA_DIR, 'annotations', 'person_keypoints_val2017.json')
 IMAGE_DIR_TRAIN = os.path.join(DATA_DIR, 'images/train2017')
 IMAGE_DIR_VAL = os.path.join(DATA_DIR, 'images/val2017')
 
@@ -33,12 +35,12 @@ def parse_args():
     )
     parser.add_argument('--cfg',
                         help='experiment configure file name',
-                        default = 'experiments/vgg19_368x368_sgd_lr1.yaml',
-                        type=str)    
+                        default='experiments/vgg19_368x368_sgd_lr1.yaml',
+                        type=str)
     parser.add_argument('opts',
                         help="Modify config options using the command-line",
                         default=None,
-                        nargs=argparse.REMAINDER)                        
+                        nargs=argparse.REMAINDER)
     parser.add_argument('-o', '--output', default=None,
                         help='output file')
     parser.add_argument('--stride-apply', default=1, type=int,
@@ -53,13 +55,13 @@ def parse_args():
     parser.add_argument('--ema', default=1e-3, type=float,
                         help='ema decay constant')
     parser.add_argument('--disable-cuda', action='store_true',
-                        help='disable CUDA')    
+                        help='disable CUDA')
     parser.add_argument('--logDir', default='/data/rtpose/', type=str, metavar='DIR',
-                    help='path to where the model saved')                                              
+                        help='path to where the model saved')
     parser.add_argument('--modelDir', default='./network/weight/', type=str, metavar='DIR',
-                    help='path to where the model saved')   
+                        help='path to where the model saved')
     parser.add_argument('--dataDir', default='./network/weight/', type=str, metavar='DIR',
-                    help='path to where the model saved')                                                                                
+                        help='path to where the model saved')
     args = parser.parse_args()
 
     # add args.device
@@ -68,9 +70,8 @@ def parse_args():
     if not args.disable_cuda and torch.cuda.is_available():
         args.device = torch.device('cuda')
         args.pin_memory = True
-        
-    return args
 
+    return args
 
 
 args = parse_args()
@@ -78,23 +79,24 @@ update_config(cfg, args)
 print("Loading dataset...")
 # load train data
 preprocess = transforms.Compose([
-        transforms.Normalize(),
-        transforms.RandomApply(transforms.HFlip(), 0.5),
-        transforms.RescaleRelative(),
-        transforms.Crop(cfg.DATASET.IMAGE_SIZE),
-        transforms.CenterPad(cfg.DATASET.IMAGE_SIZE),
-    ])
+    transforms.Normalize(),
+    transforms.RandomApply(transforms.HFlip(), 0.5),
+    transforms.RescaleRelative(),
+    transforms.Crop(cfg.DATASET.IMAGE_SIZE),
+    transforms.CenterPad(cfg.DATASET.IMAGE_SIZE),
+])
 
 # model
 rtpose_vgg = get_model(trunk='vgg19')
 # load pretrained
 use_vgg(rtpose_vgg)
 
+
 class rtpose_lightning(pl.LightningModule):
 
     def __init__(self, preprocess, target_transforms, model):
         super(rtpose_lightning, self).__init__()
-        
+
         self.preprocess = preprocess
         self.model = model
         self.target_transforms = target_transforms
@@ -108,37 +110,38 @@ class rtpose_lightning(pl.LightningModule):
         total_loss = 0
         for j in range(6):
             pred1 = saved_for_loss[2 * j]
-            pred2 = saved_for_loss[2 * j + 1] 
-
+            pred2 = saved_for_loss[2 * j + 1]
 
             # Compute losses
             loss1 = F.mse_loss(pred1, paf_target, reduction='mean')
-            loss2 = F.mse_loss(pred2, heatmap_target, reduction='mean') 
+            loss2 = F.mse_loss(pred2, heatmap_target, reduction='mean')
 
             total_loss += loss1
             total_loss += loss2
 
             # Get value from Variable and save for log
             loss_dict['paf'] = loss1.unsqueeze(0)
-            loss_dict['heatmap'] = loss2.unsqueeze(0)       
+            loss_dict['heatmap'] = loss2.unsqueeze(0)
 
-        loss_dict['loss'] = total_loss.unsqueeze(0)            
-              
-        loss_dict['max_heatmap'] = torch.max(pred2.data[:, :-1, :, :]).unsqueeze(0)
-        loss_dict['min_heatmap'] = torch.min(pred2.data[:, :-1, :, :]).unsqueeze(0)
+        loss_dict['loss'] = total_loss.unsqueeze(0)
+
+        loss_dict['max_heatmap'] = torch.max(
+            pred2.data[:, :-1, :, :]).unsqueeze(0)
+        loss_dict['min_heatmap'] = torch.min(
+            pred2.data[:, :-1, :, :]).unsqueeze(0)
         loss_dict['max_paf'] = torch.max(pred1.data).unsqueeze(0)
-        loss_dict['min_paf'] = torch.min(pred1.data).unsqueeze(0)     
-    
+        loss_dict['min_paf'] = torch.min(pred1.data).unsqueeze(0)
+
         return loss_dict
-        
+
     def training_step(self, batch, batch_nb):
         img, heatmap_target, paf_target = batch
         saved_for_loss = self.forward(img)
         loss_dict = self.l2_loss(saved_for_loss, heatmap_target, paf_target)
         output = {
-            'loss': loss_dict['loss'], # required
-            'prog': loss_dict # optional
-        }        
+            'loss': loss_dict['loss'],  # required
+            'prog': loss_dict  # optional
+        }
         return output
 
     def validation_step(self, batch, batch_nb):
@@ -147,25 +150,26 @@ class rtpose_lightning(pl.LightningModule):
         loss_dict = self.l2_loss(saved_for_loss, heatmap_target, paf_target)
         loss_dict['val_loss'] = loss_dict['loss']
         return loss_dict
-        
+
     def validation_end(self, outputs):
-        output_dict = OrderedDict()       
+        output_dict = OrderedDict()
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         output_dict['avg_val_loss'] = avg_loss
-      
+
         return output_dict
 
     def configure_optimizers(self):
-    
+
         optimizer = torch.optim.SGD(self.parameters(), lr=cfg.TRAIN.LR,
-                           momentum=cfg.TRAIN.MOMENTUM,
-                           weight_decay=cfg.TRAIN.WD,
-                           nesterov=cfg.TRAIN.NESTEROV)    
-                             
+                                    momentum=cfg.TRAIN.MOMENTUM,
+                                    weight_decay=cfg.TRAIN.WD,
+                                    nesterov=cfg.TRAIN.NESTEROV)
+
         #scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.8, patience=5, \
         #            verbose=True, threshold=0.0001, threshold_mode='rel', cooldown=3,\
-        #            min_lr=0, eps=1e-08)   
-        scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[100,150], gamma=0.1)        
+        #            min_lr=0, eps=1e-08)
+        scheduler = lr_scheduler.MultiStepLR(
+            optimizer, milestones=[100, 150], gamma=0.1)
         return [[optimizer], [scheduler]]
 
     @pl.data_loader
@@ -177,14 +181,14 @@ class rtpose_lightning(pl.LightningModule):
             image_transform=transforms.image_transform_train,
             target_transforms=self.target_transforms,
             n_images=None,
-            ) for item in cfg.DATASET.TRAIN_ANNOTATIONS]
+        ) for item in cfg.DATASET.TRAIN_ANNOTATIONS]
 
         train_data = torch.utils.data.ConcatDataset(train_datas)
-        
+
         train_loader = torch.utils.data.DataLoader(
             train_data, batch_size=cfg.TRAIN.BATCH_SIZE_PER_GPU*len(cfg.GPUS), shuffle=True,
             pin_memory=cfg.PIN_MEMORY, num_workers=cfg.WORKERS, drop_last=True)
-            
+
         return train_loader
 
     @pl.data_loader
@@ -200,7 +204,7 @@ class rtpose_lightning(pl.LightningModule):
         val_loader = torch.utils.data.DataLoader(
             val_data, batch_size=cfg.TEST.BATCH_SIZE_PER_GPU*len(cfg.GPUS), shuffle=False,
             pin_memory=cfg.PIN_MEMORY, num_workers=cfg.WORKERS, drop_last=True)
-        
+
         return val_loader
 
     @pl.data_loader
@@ -216,10 +220,11 @@ class rtpose_lightning(pl.LightningModule):
         val_loader = torch.utils.data.DataLoader(
             val_data, batch_size=cfg.TEST.BATCH_SIZE_PER_GPU*len(cfg.GPUS), shuffle=False,
             pin_memory=cfg.PIN_MEMORY, num_workers=cfg.WORKERS, drop_last=True)
-        
+
         return val_loader
 
-model = rtpose_lightning(preprocess, target_transforms=None, model = rtpose_vgg)
+
+model = rtpose_lightning(preprocess, target_transforms=None, model=rtpose_vgg)
 exp = Experiment(save_dir=cfg.LOG_DIR)
 
 # callbacks
@@ -239,11 +244,10 @@ checkpoint = ModelCheckpoint(
     mode='min'
 )
 
-trainer = Trainer(experiment=exp, \
-                  max_nb_epochs=cfg.TRAIN.EPOCHS, \
-                  gpus=list(cfg.GPUS), \
+trainer = Trainer(experiment=exp,
+                  max_nb_epochs=cfg.TRAIN.EPOCHS,
+                  gpus=list(cfg.GPUS),
                   checkpoint_callback=checkpoint,
                   early_stop_callback=early_stop)
 
 trainer.fit(model)
-          
